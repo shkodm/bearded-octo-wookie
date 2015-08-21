2b79cb520858dd05d954ef6b8a478a296b4f760f
shkodm <dmshko@gmail.com>
2015-08-21 12:57:38 +0200

    New

--- BasicPrograms/lbm.py
+++ BasicPrograms/lbm.py
@@ -32,29 +32,30 @@ x1 = 35.
 y1 = 10.
 
 nx0 = 50
-nx1 = 50
 
-ny0 = 100
-ny1 = 50
+ny0 = 50
 
-nt = 10000
+nt = 100
 
 e0 = lbm.e
 e = lbm.e
 e_opp = lbm.e_opp
 forAll9do = lbm.forAll9do
 W = lbm.W
-x0,y0 = np.meshgrid(np.linspace(x0,x0+nx0*dx0,nx0), np.linspace(y0,y0+ny0*dx0,ny0))
-
-
-
+x0,y0 = np.meshgrid(np.linspace(x0,x0+(nx0 - 1)*dx0,nx0),np.linspace(y0,y0+(ny0-1)*dx0,ny0))
+x0=x0.T
+y0=y0.T
 
 
 ################################################################
 ## INIT GRID 0
 iy0n = list()
 ix0n = list()
-iy00,ix00 = np.meshgrid(np.arange(nx0), np.arange(ny0))
+ix00,iy00 = np.meshgrid(np.arange(nx0), np.arange(ny0))
+ix00 = ix00.T
+iy00 = iy00.T
+
+print ix00.shape
 #==============================================================================
 # bx0 = list()
 # by0 = list()
@@ -68,8 +69,8 @@ iy00,ix00 = np.meshgrid(np.arange(nx0), np.arange(ny0))
         
 
 for i in range(0,9):
-    ixt = np.roll(np.roll(ix00[:,:],shift=e0[i][0],axis=0),shift=e0[i][1],axis=1)
-    iyt = np.roll(np.roll(iy00[:,:],shift=e0[i][0],axis=0),shift=e0[i][1],axis=1)
+    ixt = np.roll(np.roll(ix00[:,:],shift=-e0[i][0],axis=0),shift=-e0[i][1],axis=1)
+    iyt = np.roll(np.roll(iy00[:,:],shift=-e0[i][0],axis=0),shift=-e0[i][1],axis=1)
     ix0n.append(ixt)
     iy0n.append(iyt)
     
@@ -77,17 +78,21 @@ ix0= np.array(ix0n)
 iy0= np.array(iy0n)
 
 
-f_in0 = np.zeros((ny0,nx0,9))
+f_in0 = np.zeros((nx0,ny0,9))
 f_out0 = np.zeros_like(f_in0)
 
 
 
 
-def init(_i, _W, _rho, _fin):
-    _fin[:,:,_i] =  _W[_i] * _rho[:,:]
+def init(_i, _W, _e, _rho, _U,  _fin):
+    cu  = 3. * ( _U[:,:,0] * _e[_i,0] + _U[:,:,1] * _e[_i,1])
+    feq1 = _W[_i] * _rho[:,:] * (1. + cu[:,:] + 0.5*cu[:,:]*cu[:,:] - (3./2.) * ( _U[:,:,0]**2 + _U[:,:,1]**2 ) )
+
+    _fin[:,:,_i] =  feq1
 
 
 def stream(_i, _ox, _oy, _fin, _fou):
+    
     _fin[:,:,_i] = _fou[_ox[_i],_oy[_i],_i] 
     
 def BGK(_i, _U, _rho, _e, _W, _tau, _F, _fin, _fou):
@@ -129,12 +134,10 @@ meq0, M0 = MRT.getMRT(e0)
 M0 = np.array(M0, dtype=float)
 M0inv = np.linalg.inv(M0)
 
-rho0 = np.ones_like(x0)
-
-forAll9do(init, W, rho0, f_in0)
+rho0 = np.ones((nx0,ny0))
 
 
-U0 = np.zeros((ny0,nx0,2))
+U0 = np.zeros((nx0,ny0,2))
 
 
 _tau = 1.
@@ -144,53 +147,62 @@ F0[:,:,0] = 0.0000
 
 
 #==============================================================================
-# BOUNDARY CONDYTIONS
+# BOUNDARY CONDITIONS
 #==============================================================================
 
 
+U = 0.001 #  x - component of velocity
+V = 0.001 # y  - component of velocity
 
-BC_fun_Left, BC_A_Left = BC.getRhoBc([0,1], 1.)
-#BC_fun_Right, BC_A_Right = BC.getRhoBc([0,-1], 1.)
+BC_fun_Left,BC_A_Left = BC.getUBc([0,1],0.,0.)
 
-BC_fun_Right, BC_A_Right = BC.getUBc([0,-1], 0., 0.)
+BC_fun_Right, BC_A_Right = BC.getUBc([0,-1], 0.,0.)
 
-Uinf = 0.00
-#BC_fun_Left, BC_A_Left = BC.getUBc([0,1], 0., Uinf)
-#BC_fun_Right, BC_A_Right = BC.getUBc([0,-1], 0., Uinf)
+BC_fun_Top, BC_A_Top = BC.getUBc([1,0], 0.0, 0.0)
 
+BC_fun_Bottom, BC_A_Bottom = BC.getUBc([-1,0], 0., 0.)
 
-
-#BC_fun_Bottom, BC_A_Bottom = BC.getRhoBc([1,0], 1., Uinf)
-#BC_fun_Top, BC_A_Top = BC.getRhoBc([-1,0], 1., Uinf)
-BC_fun_Top, BC_A_Top = BC.getUBc([1,0], 0., 0.000)
-BC_fun_Bottom, BC_A_Bottom = BC.getUBc([-1,0], 0., Uinf)
-
-print BC_A_Bottom.shape
-
-
-
-ubc = lambda (x): (1. - x / nx0) * 0.0001
-
+#test = lambda x, y: ( ( ((x/nx0)) * (((x/(nx0-1.))-1) ))*0.0001,0)
+test = lambda x, y: (y*0.01, 0*x)
+#test2 = lambda(x): (0,0)
+U0[:,:,0],U0[:,:,1] = test(x0,y0)
 
 BC_A_Right = list()            
-for x in y0[:,-1]: 
-    print ubc(x)
-    BC_A_Right.append( BC_fun_Right(0, ubc(x) ) )
-BC_A_Right = np.array(BC_A_Right)                    
+for x,y in zip(x0[-1,:],y0[-1,:]): 
+    BC_A_Right.append(BC_fun_Right(*test(x,y)[:]))   
+BC_A_Right = np.array(BC_A_Right)
+
+BC_A_Left = list()            
+for x,y in zip(x0[0,:],y0[0,:]): 
+   BC_A_Left.append(BC_fun_Left(*test(x,y)[:]))
+BC_A_Left = np.array(BC_A_Left)                 
+
+BC_A_Top = list()            
+for x,y in zip(x0[:,-1],y0[:,-1]): 
+    BC_A_Top.append(BC_fun_Top(*test(x,y)[:]))
+BC_A_Top = np.array(BC_A_Top)
+    
+BC_A_Bottom = list()            
+for x,y in zip(x0[:,0],y0[:,0]): 
+    BC_A_Bottom.append(BC_fun_Bottom(*test(x,y)[:]))
+BC_A_Bottom = np.array(BC_A_Bottom) 
+
+
+forAll9do(init, W, e, rho0, U0,  f_in0)
 
 
 for it in range(nt):
     print it
 
     ### inlet
-    f_in0[:,0,:] = applyBC(BC_A_Left, f_in0[:,0,:])
+    #f_in0[:,0,:] = applyWideBC(BC_A_Bottom, f_in0[:,0,:])
     ### outlet
-    f_in0[:,-1,:] = applyWideBC(BC_A_Right, f_in0[:,-1,:])
+    #f_in0[:,-1,:] = applyWideBC(BC_A_Top, f_in0[:,-1,:])
     
     ### bottom
-    f_in0[0,:,:] = applyBC(BC_A_Bottom, f_in0[0,:,:])
+    f_in0[0,:,:] = applyWideBC(BC_A_Left, f_in0[0,:,:])
     ### top
-    f_in0[-1,:,:] = applyBC(BC_A_Top, f_in0[-1,:,:])
+    f_in0[-1,:,:] = applyWideBC(BC_A_Right, f_in0[-1,:,:])
     
 
     rho0[:,:] = np.sum( f_in0[:,:,:], 2 )
@@ -203,36 +215,44 @@ for it in range(nt):
     U0[:,:,1] =  U0[:,:,1] / rho0
    
 
-    #forAll9do(BGK, U0, rho0, e, W, tau0, F0, f_in0, f_out0)
-    
-    for i,f in enumerate(f_in0):
-        f = f.T
-        m =  (M0.dot(f))
-        meq_0 = meq0(rho0[i,:], U0[i,:,0], U0[i,:,1])
-        meq_1 = meq0(rho0[i,:], U0[i,:,0] + F0[i,:,0], U0[i,:,1] + F0[i,:,1])
-        f_out = M0inv.dot( m + (_tau) * ( meq_0 - m ) + (meq_1 - meq_0) )
-        f_out0[i,:,:] = f_out.T
+    forAll9do(BGK, U0, rho0, e, W, tau0, F0, f_in0, f_out0)
+#==============================================================================
+#     
+#     for i,f in enumerate(f_in0):
+#         f = f.T
+#         m =  (M0.dot(f))
+#         meq_0 = meq0(rho0[i,:], U0[i,:,0], U0[i,:,1])
+#         meq_1 = meq0(rho0[i,:], U0[i,:,0] + F0[i,:,0], U0[i,:,1] + F0[i,:,1])
+#         f_out = M0inv.dot( m + (_tau) * ( meq_0 - m ) + (meq_1 - meq_0) )
+#         f_out0[i,:,:] = f_out.T
+#==============================================================================
  
  
 
    
-    if it > 0 and np.mod(it, 100) == 0:
+    if it > 0 and np.mod(it, nt/10) == 0:
         iii = 0
-        plt.contourf(x0,y0,U0[:,:,0]**2 + U0[:,:,1]**2)
-        plt.colorbar()        
-        plt.quiver(x0,y0,U0[:,:,1], U0[:,:,0])
+        T= np.array(test(x0,y0))
+        print T.shape
+        plt.contourf(x0,y0,(U0[:,:,0]-T[0,:,:])**2 + (U0[:,:,1]-T[1,:,:])**2) 
+        plt.colorbar()
+#        plt.streamplot(x0,y0.T,U0[:,:,1],U0[:,:,0])   
+        plt.quiver(x0,y0,1000.*U0[:,:,0], 1000.*U0[:,:,1],scale_units  = 'width')
         
 
-        plt.figure()
-        plt.plot(U0[:,0,1])
-        plt.plot(U0[:,0,0])
+#        plt.figure()
+#        plt.plot(U0[:,0,1])
+#        plt.plot(U0[:,0,0])
         
         plt.show()
 
     #forAll9do(BB, bx0, by0, e_opp, f_in0, f_out0)
     
     forAll9do(stream, ix0, iy0, f_in0, f_out0)
-
-plt.contourf(x0,y0,U0[:,:,0])
-
+#print U0[:,0,0]
+print U0[:,ny0 / 2,0]
+#print (U0[0,:,1],U0[-1,:,1])
+print (U0[0,:,0],U0[-1,:,0])
+#plt.contourf(x0,y0,U0[:,:,0])
+plt.imshow(U0[:,:,0], interpolation='nearest')
 plt.show()
